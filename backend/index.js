c20c7985b84f9f3215596e48bb17f158785ff5de
kielFernandes <kielfrndes@gmail.com>
2025-04-03 00:04:49 +0700

    Perubahan baru pada backend

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 const TodolistRouting = require("./Controller/TodolistRouting")
+const GambarRouting = require("./Controller/GambarRouting")
 
+const path = require("path")
 
 mongoose.connect(mongoString);
 const db = mongoose.connection;
@@ -22,6 +24,8 @@ db.on("error",(err) => {
 })
 
 app.use("/todo", TodolistRouting);
+app.use("/uploads", express.static(path.join(__dirname,"uploads")))
+app.use("/images",GambarRouting)
 
 app.listen(3001,() => {
     console.log("Server sudah berjalan")
